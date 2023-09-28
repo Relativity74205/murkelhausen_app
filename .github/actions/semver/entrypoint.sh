1ed4a6782dfa3ca8fceb8bfc373f1ac57e80688b
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2023-09-28 22:03:45 +0200

    ci: removed .git folder from Dockerfile

--- .github/actions/semver/entrypoint.sh
+++ .github/actions/semver/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
-python semver_tagging.py
+ls -la
+ls -la /
+
+python /semver_tagging.py
 
 echo "next_tag=${NEXT_TAG}" >> $GITHUB_OUTPUT
 echo "changelog_delta=${CHANGELOG}" >> $GITHUB_OUTPUT
