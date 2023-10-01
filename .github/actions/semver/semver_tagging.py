20714ecd13a2e67f8162a081e0677aa8cd773795
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2023-10-01 22:44:38 +0200

    ci: switched from git to github library

--- .github/actions/semver/semver_tagging.py
+++ .github/actions/semver/semver_tagging.py
@@ -1,13 +1,9 @@
 import os
 from dataclasses import dataclass
+from datetime import datetime
 from enum import StrEnum, auto
 
-from git import Repo
-
-from pathlib import Path
-
-
-repo = Repo.init(Path.cwd())
+from github import Github, Auth, Repository
 
 
 @dataclass
@@ -45,25 +41,35 @@ class UpgradeType(StrEnum):
     NONE = auto()
 
 
-def get_last_tag(repo: Repo) -> Tag | None:
-    for tag in repo.tags:
-        print(tag)
+def get_github_repo() -> Repository:
+    github_token = os.environ["GITHUB_TOKEN"]
+    auth = Auth.Token(github_token)
+
+    g = Github(auth=auth)
+
+    return g.get_repo("Relativity74205/murkelhausen_app")
+
+
+def get_last_tag(repo: Repository) -> tuple[Tag | None, datetime | None]:
+    tags = repo.get_tags()
     try:
-        last_tag_string = repo.tags[-1].name
-        return Tag.from_tag_name(last_tag_string)
+        last_tag = sorted(tags, key=lambda x: x.name)[-1]
+        return Tag.from_tag_name(last_tag.name), last_tag.commit.commit.author.date
     except IndexError:
-        return None
+        return None, None
 
 
-def get_commit_messages_since_tag(repo: Repo, last_tag: Tag) -> list[str]:
-    if last_tag is None:
-        commits_since_tag = list(repo.iter_commits("HEAD"))
+def get_commit_messages_since_tag(
+    repo: Repository, last_tag_datetime: datetime
+) -> list[str]:
+    if last_tag_datetime is None:
+        commits_since_tag = repo.get_commits()
     else:
-        commits_since_tag = list(repo.iter_commits(f"{last_tag}..HEAD"))
-    return [commit.message for commit in commits_since_tag]
+        commits_since_tag = repo.get_commits(since=last_tag_datetime)
+    return [commit.commit.message for commit in commits_since_tag]
 
 
-def get_commit_summaries_since_tag(repo: Repo, last_tag: Tag) -> list[str]:
+def get_commit_summaries_since_tag(repo: Repository, last_tag: Tag) -> list[str]:
     if last_tag is None:
         commits_since_tag = list(repo.iter_commits("HEAD"))
     else:
@@ -118,17 +124,17 @@ def calculate_next_tag(commit_messages: list[str], last_tag: Tag) -> str:
 
 
 def main():
-    print(f"Start with {repo=}")
-    last_tag = get_last_tag(repo)
-    print(f"{last_tag=}")
-    commit_messages = get_commit_messages_since_tag(repo, last_tag)
-    exit(1)
+    print("Start.")
+    github_repo = get_github_repo()
+    last_tag, last_tag_datetime = get_last_tag(github_repo)
+    print(f"{last_tag=} with {last_tag_datetime=}")
+    commit_messages = get_commit_messages_since_tag(github_repo, last_tag_datetime)
 
     next_tag = calculate_next_tag(commit_messages, last_tag)
+    print(f"{next_tag=}")
 
     os.environ["NEXT_TAG"] = next_tag
-    commit_summaries = get_commit_summaries_since_tag(repo, last_tag)
-    os.environ["CHANGELOG"] = "\n".join(commit_summaries)
+    os.environ["CHANGELOG"] = "\n".join(commit_messages)
 
 
 if __name__ == "__main__":
