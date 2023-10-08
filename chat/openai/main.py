73b9165f355ac1e18620306ea5984c2787f1f98d
Arkadius Schuchhardt <34404734+Relativity74205@users.noreply.github.com>
2023-10-08 11:18:27 +0200

    fix: added OPENAI Key to Dockerfile (#19)

--- chat/openai/main.py
+++ chat/openai/main.py
@@ -3,14 +3,16 @@ from enum import StrEnum, auto
 import openai
 
 
-image_resp = openai.Image.create(
-    prompt="two dogs playing chess, oil painting", n=4, size="512x512"
-)
-
-
 class OpenAIModel(StrEnum):
     GPT35TURBO = "gpt-3.5-turbo"
     GPT4 = "gpt-4"
+
+
+def image_generation():
+    raise NotImplementedError
+    image_resp = openai.Image.create(
+        prompt="two dogs playing chess, oil painting", n=4, size="512x512"
+    )
 
 
 def generate_chat_completion(
@@ -24,6 +26,7 @@ def generate_chat_completion(
     - [ ] count usage tokens from answer
     - [ ] evaluate finish_reason
     """
+    # TODO add some kind of error handling, especially if no API key is set
     completion = openai.ChatCompletion.create(
         model=model, messages=[{"role": "user", "content": input_message}]
     )
--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def home_view(request, *args, **kwargs):
-    return render(request, "pages/main.html", {})
+    import importlib.metadata
+
+    __version__ = importlib.metadata.version("murkelhausen-app")
+    return render(request, "pages/main.html", {"version": __version__})
