2864249c464845c35ad7d30f33e46072cce5cb6c
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2023-11-08 22:05:19 +0100

    feat: added current, forecast, past tabs to weather info

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 
 def home_view(request, *args, **kwargs):
+    return render(request, "pages/main.html")
+
+
+def get_murkelhausen_version(request) -> dict:
     import importlib.metadata
 
     __version__ = importlib.metadata.version("murkelhausen-app")
-    return render(request, "pages/main.html", {"version": __version__})
+
+    return {"version": __version__}
