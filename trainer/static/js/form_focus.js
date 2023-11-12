e528b7eb81d165c771341685c01de1ec5632c93f
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2023-11-12 19:15:04 +0100

    fix: cleanup of navbar for all apps

--- trainer/static/js/form_focus.js
+++
@@ -1,9 +0,0 @@
-// form_focus.js
-function setFocusOnField(fieldId) {
-    document.addEventListener("DOMContentLoaded", function() {
-        var inputField = document.getElementById(fieldId);
-        if (inputField) {
-            inputField.focus();
-        }
-    });
-}
