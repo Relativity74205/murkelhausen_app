9b5b8fcc77b69132c1a0d7c7e7174273af962981
Arkadius Schuchhardt <34404734+Relativity74205@users.noreply.github.com>
2023-11-14 23:05:17 +0100

    feat: added MHEG MVP (#30)
    * feat: added MHEG MVP
    
    * fix: babel import

--- murkelhausen_info/urls.py
+++ murkelhausen_info/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.start, name="start"),
     path("weather/", views.WeatherView.as_view(), name="weather"),
     path("power/", views.power, name="power"),
-    path("departures/", views.DepartureView.as_view(), name="show_departures"),
+    path("departures/", views.DepartureView.as_view(), name="departures"),
+    path("muell/", views.MuellView.as_view(), name="muell"),
 ]
